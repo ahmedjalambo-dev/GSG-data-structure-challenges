886de09ac77598b8719f841acc11e21d56bde8d5
Jalambo-Dev <ahmedjalambo.dev@gmail.com>
2025-05-11 22:18:17 +0300

    Initialize Dart project with basic structure and configuration files

---
+++ bin/gsg_data_structure_challenges.dart
@@ -0,0 +1,5 @@
+import 'package:gsg_data_structure_challenges/gsg_data_structure_challenges.dart' as gsg_data_structure_challenges;
+
+void main(List<String> arguments) {
+  print('Hello world: ${gsg_data_structure_challenges.calculate()}!');
+}
---
+++ test/gsg_data_structure_challenges_test.dart
@@ -0,0 +1,8 @@
+import 'package:gsg_data_structure_challenges/gsg_data_structure_challenges.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
